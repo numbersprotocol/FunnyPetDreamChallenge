2db337b57cf125344f1a54b718bcba3043438497
DaYuan <sduum82@gmail.com>
2021-06-28 03:59:55 +0800

    add Description i18n.

--- src/i18n.js
+++ src/i18n.js
@@ -10,14 +10,28 @@ const resources = {
       "Welcome to React": "Welcome to React and react-i18next",
       "HashTag": "#FunnyPetDreamChallenge",
       "Voting": "Voting and Raffle Information",
-      "Demo": "Charity Demo Vidoe", 
+      "Demo": "Charity Demo Vidoe",
       "DemoSet": "Demo Videos From Taipei Zoo",
-      "EventName": "Funny Pet Dream", 
+      "EventName": "Funny Pet Dream",
       "Entries": "Entries",
       "VotingPeriod": "Voting Period",
-      "HowToVote": "How to vote", 
-      "": "", 
-      "": "",
+      "HowToVote": "How to vote",
+      "EventHomepage": "Event homepage",
+      "click": "You can click",
+      "share": "share this page ～",
+      "link": "You can click ☝️ #Tag ☝️ ,share this page ～",
+      'VotingDescription':`<p class="I155:22;107:587">
+      1. Choose one of the 15 entries you like the most, and click "Like"<br>
+      2. Follow @only.truephotos<br>
+      3. All set!<br>
+      <p>After the event, 5 people from the voting audience will be selected at
+        <p>random, and each will receive a 30 USDT bonus.</p>
+        Voting is limited to IG accounts, and there is no limit to the number
+        of votes (if you like every work, you are welcome to support it), but
+        each account has only one chance to draw a lottery.</p>
+    </p>`,
+      "userMessagesUnread": " 哈偻 <1>{{name}}</1>, you have {{count}} unread message. <5>Go to message</5>.",
+      "userMessagesUnread_plural": "Hello <1>{{name}}</1>, you have {{count}} unread messages.  <5>Go to messages</5>.",
     }
   },
   zh: {
@@ -25,22 +39,19 @@ const resources = {
       "Welcome to React": "Bienvenue à React et react-i18next",
       "HashTag": "#FunnyPetDreamChallenge",
       "Voting": "投票辦法",
-      "Demo": "公益示範賽", 
-      "DemoSet": "臺北市立動物園公益示範賽", 
+      "Demo": "公益示範賽",
+      "DemoSet": "臺北市立動物園公益示範賽",
       "EventName": "廢寵入選睡播大挑戰",
-      "Entries": "入選作品", 
+      "Entries": "入選作品",
       "VotingPeriod": "投票時間",
-      "HowToVote": "參加辦法", 
-
-      "": '從下方入選的 15 個參賽作品中選一個最喜歡的，點選 " 愛心 "',
-      "": "追蹤 IG:@only.truephotos 即完成投票 活動結束後將舉行抽獎，從參與投票的觀眾中抽出 5 名，各獲 30 USDT 獎 金。", 
-      "": "投票僅限 IG 帳號，不限投票數量 (如果每一個作品都喜歡，歡迎都愛心支持)， 唯每個帳號僅有一次抽獎機會。",
-  
- 
-      "": "",
-      "": "", 
-      "": "",
-    
+      "HowToVote": "參加辦法",
+      "EventHomepage": "活動主頁",
+      "click": "點擊右側",
+      "share": "分享活動",
+      "link": "點擊上方 ☝️ #Tag ☝️ ,分享活動 ～",
+      'VotingDescription':`<p class="I155:22;107:587">1. 從下方入選的 15 個參賽作品中選一個最喜歡的，點選 " 愛心 "<br/>2. 追蹤 IG:@only.truephotos 即完成投票 活動結束後將舉行抽獎，從參與投票的觀眾中抽出 5 名，各獲 30 USDT 獎 金。投票僅限 IG 帳號，不限投票數量 (如果每一個作品都喜歡，歡迎都愛心支持)， 唯每個帳號僅有一次抽獎機會。</p>`,
+      "userMessagesUnread": " 哈偻 <1>{{name}}</1>, you have {{count}} unread message. <5>Go to message</5>.",
+      "userMessagesUnread_plural": "  哈偻 <1>{{name}}</1>, you have {{count}} unread messages.  <5>Go to messages</5>.",
     }
   }
 };
@@ -50,7 +61,7 @@ i18n
   .init({
     resources,
     lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    fallbackLng: 'en',     
+    fallbackLng: 'en',
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
     interpolation: {
--- src/page/Description/Description.js
+++ src/page/Description/Description.js
@@ -1,31 +1,40 @@
 import './Description.css';
-import { withTranslation } from 'react-i18next';
+import { withTranslation, Trans } from 'react-i18next';
 
 function Description({ t }) {
+  const { name } = 'person';
+  const count = 4;
   return (
     // <div className="Description" class="grid place-items-center  h-screen-1/2 mx-auto p-10 my-20 sm:w-max sm:my-auto rounded-xl space-y-5 text-center">
     <div className="Description" class="flex flex flex-col items-center	 place-items-center font-mono  mx-auto py-5 my-20 text-center sm:text-left">
-      <div className="DemoSetTitle" class=" text-2xl sm:text-4xl font-nanifont my-12 p-4 text-center rounded-2xl m-5 z-40 -mb-16 violet text-white shadow-gl w-5/12	">
-      {t('Voting')}
+      <div className="DemoSetTitle" class=" text-4xl sm:text-4xl font-nanifont my-12 p-4 text-center rounded-2xl m-5 z-40 -mb-16 violet text-white shadow-gl w-7/12	sm:w-5/12">
+        {t('Voting')}
       </div>
       <div class="relative py-3 w-9/12 mx-auto my-10" >
         <div class="absolute inset-0 violet  bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-0 -rotate-3 rounded-3xl"></div>
         <div class="relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:px-20">
           <div class="w-full md:w-auto">
-          <h3 class="text-2xl">{t('VotingPeriod')} : 2021/7/12 10:00 - 2021/7/16 23:59 (UTC+8)</h3>
+            <h3 class="text-2xl">{t('VotingPeriod')} : 2021/7/12 10:00 - 2021/7/16 23:59 (UTC+8)</h3>
             <h3 class="text-2xl">{t('HowToVote')}:</h3>
-            <p class="I155:22;107:587">
+            {/* <p class="I155:22;107:587">
               1. 從下方入選的 15 個參賽作品中選一個最喜歡的，點選 " 愛心 "
-              <ul/>
-              <br/>
+              <ul />
+              <br />
               2. 追蹤 IG:@only.truephotos 即完成投票 活動結束後將舉行抽獎，從參與投票的觀眾中抽出 5 名，各獲 30 USDT 獎 金。
               投票僅限 IG 帳號，不限投票數量 (如果每一個作品都喜歡，歡迎都愛心支持)， 唯每個帳號僅有一次抽獎機會。
+            </p> */}
+            <Trans i18nKey="VotingDescription">
+            </Trans>
+            <p>
+              <Trans i18nKey="userMessagesUnread" count={count}>
+                Hello <strong title={t('nameTitle')}>{{ name }}</strong>, you have {{ count }} unread message. .
+              </Trans>
             </p>
           </div>
         </div>
       </div>
-      <div className="DemoSetTitle" class=" text-2xl sm:text-4xl font-nanifont my-12 p-4 text-center rounded-2xl m-5 z-40 -mb-16 violet text-white shadow-gl w-5/12	">
-      {t('Demo')}
+      <div className="DemoSetTitle" class=" text-4xl sm:text-6xl font-nanifont my-12 p-4 text-center rounded-2xl m-5 z-40 -mb-16 violet text-white shadow-gl w-7/12	sm:w-5/12">
+        {t('Demo')}
       </div>
       <div class="relative py-3  w-9/12 mx-auto my-10" >
         <div class="absolute inset-0 violet   bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-0 -rotate-3 rounded-3xl"></div>
@@ -33,7 +42,7 @@ function Description({ t }) {
           <div class="w-full md:w-auto">
             <h3 class="text-2xl">{t('VotingPeriod')} : 2021/7/12 10:00 - 2021/7/16 23:59 (UTC+8)</h3>
             <p class="I155:22;107:587">
-              本次特別邀請臺北市立動物園參與公益示範賽，歡迎大家也給示範賽"愛心"， 投票完畢記得追蹤 IG 帳號:@only.truephotos 投給示範賽作品也可以抽獎唷!<ul/> <br/>示範賽獲得的愛心加總數目將化成實際金額捐出款項給臺北市立動物園。 <ul/><br/>示範賽作品將發行 NFT 進行公益拍賣，拍賣所得全額捐出給臺北市立動物園， 協助提昇圈養動物的照養、醫療、繁殖和族群管理技術，及原生棲地內的野生 動物研究。
+              本次特別邀請臺北市立動物園參與公益示範賽，歡迎大家也給示範賽"愛心"， 投票完畢記得追蹤 IG 帳號:@only.truephotos 投給示範賽作品也可以抽獎唷!<ul /> <br />示範賽獲得的愛心加總數目將化成實際金額捐出款項給臺北市立動物園。 <ul /><br />示範賽作品將發行 NFT 進行公益拍賣，拍賣所得全額捐出給臺北市立動物園， 協助提昇圈養動物的照養、醫療、繁殖和族群管理技術，及原生棲地內的野生 動物研究。
             </p>
           </div>
         </div>
